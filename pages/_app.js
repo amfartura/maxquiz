17c2c275509ffab688177f725603e1fb6d9af5c8
Alex Maximiano <amfartura@protonmail.com>
2021-01-30 22:25:55 -0300

    Correção

--- pages/_app.js
+++ pages/_app.js
@@ -7,7 +7,6 @@ const GlobalStyle = createGlobalStyle`
     box-sizing: border-box;
   }
   body {
-    @import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
     margin: 0;
     padding: 0;
     /* New styles */
